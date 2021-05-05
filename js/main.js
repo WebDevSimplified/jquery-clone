99915f8fe8a5ce848e0282419539a78df1324e8b
Web Dev Simplified <webdevsimplified@gmail.com>
2021-05-05 11:13:55 -0500

    Finish Video

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(document).on("click", ".next", function () {
+    const currentImg = $(".active")
+    const nextImg = currentImg.next()
+
+    if (nextImg.length) {
+      currentImg.removeClass("active").css("z-index", -10)
+      nextImg.addClass("active").css("z-index", 10)
+    }
+  })
+
+  $(".prev").on("click", function () {
+    const currentImg = $(".active")
+    const prevImg = currentImg.prev()
+
+    if (prevImg.length) {
+      currentImg.removeClass("active").css("z-index", -10)
+      prevImg.addClass("active").css("z-index", 10)
+    }
+  })
+})
+
+$.get({
+  url: "https://jsonplaceholder.typicode.com/todos/-1",
+  success: data => {
+    console.log("First success", data)
+  },
+})
+  .done(data => console.log("Second success", data))
+  .fail(e => console.error("Fail", e))
+  .always(() => console.log("Always"))
